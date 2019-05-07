111829ae02344aa7fd6923fd816fcd188ead6aeb
Daan Seynaeve <daan.seynaeve@openanalytics.eu>
2019-05-07 18:14:11 +0200

    add S3 interface and redis implementation [wip]

---
+++ rtq/R/S3.R
@@ -0,0 +1,57 @@
+
+#' Lightweight Reliable Task Queue - Start processing a task item
+#' @rdname leaseTask
+#' @param tq Task Queue
+#' @param blocking block until a message is available
+#' @param blockTimeout maximum amount of time to wait until a message is available
+#' @param leaseTimeout maximum amount of time (in seconds) to lease a messsage
+#' @examples \dontrun{
+#' tq <- "Task Queue backend"
+#' createTask(tq, "some imporant task")
+#' item <- leaseTask(tq)
+#' completeTask(item)
+#' }
+#' @seealso \code{\link{leaseTask}} \code{\link{completeTask}} \code{\link{createTask}}
+#' @export
+leaseTask <- function(
+    tq,
+    blocking,
+    blockTimeout,
+    leaseTimeout,
+    ...) {
+  
+  UseMethod("leaseTask")
+  
+}
+#' Lightweight Reliable Task Queue - Stop processing a task item
+#' @rdname completeTask
+#' @param tq Task Queue
+#' @param item item obtained from \code{\link{lease}}
+#' @seealso \code{\link{leaseTask}} \code{\link{completeTask}} \code{\link{createTask}}
+#' @export
+completeTask <- function(tq, item) {
+  
+  UseMethod("completeTask")
+  
+}
+#' Lightweight Reliable Task Queue - Add a task item
+#' @rdname createTask
+#' @param tq Task Queue
+#' @param item task item to add
+#' @seealso \code{\link{leaseTask}} \code{\link{completeTask}} \code{\link{createTask}}
+#' @export
+createTask <- function(tq, item) {
+  
+  UseMethod("createTask")
+  
+}
+#' Lightweight Reliable Task Queue - Check for task item availability
+#' @rdname existsTask
+#' @param tq Task Queue
+#' @seealso \code{\link{leaseTask}} \code{\link{completeTask}} \code{\link{createTask}}
+#' @export
+existsTask <- function(tq) {
+  
+  UseMethod("existsTask")
+  
+}
---
+++ rtq/R/redis.R
@@ -0,0 +1,95 @@
+
+#' Lightweight Reliable Task Queue (Redis implementation)
+#' @description A simple reliable task queue implemented via two lists
+#' @seealso https://kubernetes.io/examples/application/job/redis/rediswq.py
+#' @import redux
+#' @seealso \code{\link{leaseTask}} \code{\link{completeTask}} \code{\link{createTask}}
+#' @export
+RedisTQ <- function(redisConf, name) {
+  
+  structure(
+      list(
+          redisConf = redisConf,
+          mainQKey = paste0(name, ":", "main"),
+          procQKey = paste0(name, ":", "processing"),
+          leasePrefix = paste0(name, ":lease:"),
+          sessionId = UUIDgenerate()
+      ), class = c("RedisTQ", "TQ", "list")
+  )
+  
+}
+#' @rdname leaseTask
+#' @import redux
+#' @seealso \code{\link{RedisTQ}}
+#' @export
+leaseTask.RedisTQ <- function(
+    tq,
+    blocking = TRUE,
+    blockTimeout = 10,
+    leaseTimeout = 600) {
+  
+  r <- hiredis(tq$redisConf)
+  
+  if (blocking) {
+    item <- r$BRPOPLPUSH(tq$mainQKey, tq$procQKey, blockTimeout)
+    if (is.null(item)) warning("Timeout expired")
+  } else {
+    item <- r$RPOPLUSH(tq$mainQKey, tq$procQKey)
+  }
+  
+  if (!is.null(item)) {
+    r$SETEX(
+        key = paste0(tq$leasePrefix, digest(item, algo = "sha256")),
+        seconds = leaseTimeout,
+        tq$sessionId)
+  }
+  
+  item
+  
+}
+#' @rdname completeTask
+#' @import redux
+#' @seealso \code{\link{RedisTQ}}
+#' @export
+completeTask.RedisTQ <- function(
+    tq,
+    item) {
+  
+  r <- hiredis(tq$redisConf)
+  
+  r$LREM(tq$procQKey, item)
+  r$DEL(digest(item, algo = "sha256"))
+  
+  invisible()
+  
+}
+#' @rdname createTask
+#' @import redux
+#' @importFrom jsonlite toJSON
+#' @seealso \code{\link{RedisTQ}}
+#' @export
+createTask.RedisTQ <- function(
+    tq,
+    item) {
+  
+  if (!is.character(item)) {
+    warning("item is not a string and will be converted to JSON")
+    item <- toJSON(item)
+  }
+  
+  r <- hiredis(tq$redisConf)
+  
+  r$LPUSH(tq$mainQKey, item)
+  
+}
+#' @rdname existsTask
+#' @import redux
+#' @seealso \code{\link{RedisTQ}}
+#' @export 
+existsTask.RedisTQ <- function(tq) {
+  
+  r <- hiredis(tq$redisConf)
+  
+  r$LLEN(tq$mainQKey) > 0
+  
+}
